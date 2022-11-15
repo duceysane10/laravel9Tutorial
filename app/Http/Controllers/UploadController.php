50f25984f691b700c16d48f2d0a023d0683a9155
Abdifitah <engduceysane10@gmail.com>
2022-11-15 08:38:37 +0300

    added file uploaded lkn aan dib usoo galeen file horay loosoo uload gareeyy

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -8,7 +8,11 @@ class UploadController extends Controller
 {
     public function upload(Request $req){
         session()->flash('status', 'Image was Uploadede successful!');
-        $req->file('file')->store('images');
+        // $req->file('file')->store('images'); line kaan commentiga saaran fileka wuu upload gareenaa lkn isla file kii hadii mar labaad lasoo upload gareeyo wuu aqbalaa.
+        //  marka codeka ka hooseeyo ayaa ka wanaagsan oo celinaayo file hore loo soo upload gareeyy
+        $md5Name = md5_file($req->file('file')->getRealPath());
+        $guessExtension = $req->file('file')->guessExtension();
+        $req->file('file')->storeAs('docs',$md5Name.'.'.$guessExtension,'public');
         return redirect('upload');
     }
 }
